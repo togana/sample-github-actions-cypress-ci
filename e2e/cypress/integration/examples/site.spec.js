af51857d7aa23ccf79d79df2ac0d2299c8c181e0
togana <o.togana@gmail.com>
2021-03-31 13:31:04 +0900

    envの設定した

--- e2e/cypress/integration/examples/site.spec.js
+++ e2e/cypress/integration/examples/site.spec.js
@@ -1,8 +1,12 @@
 /// <reference types="cypress" />
 
 context('Site', () => {
+  const baseUrl = Cypress.env('baseUrl');
+  beforeEach(() => {
+    cy.visit(`${baseUrl}`)
+  });
+
   it('sholud work', () => {
-    cy.visit('http://localhost:3000')
     cy.get('a').should('have.text', 'Learn React')
   })
 })
